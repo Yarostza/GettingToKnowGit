e41a428b4843b98d4d9d578df483bcc039641dfd
Zachary Yarost <yarostza@msu.edu>
2020-03-13 10:26:16 -0400

    added last two steps

--- howtousegit.py
+++ howtousegit.py
@@ -2,3 +2,4 @@ print("To grab a clone, simply type git clone ADDRESS while in the appropriate d
 print("Next use git add FILE to add changes or files by name")
 print("Commit that cool stuff with git commit -m \"MESSAGE ABOUT WHAT WAS CHANGED\"")  # still only on our machine
 print("Finally, use git push to sync it up with the github website")
+print("And you're done")
