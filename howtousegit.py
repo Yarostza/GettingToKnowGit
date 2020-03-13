c42e3853f802579a740a3c62121fb52005f727ad
Zachary Yarost <yarostza@msu.edu>
2020-03-13 10:33:16 -0400

    added info on pull and add all in directory

--- howtousegit.py
+++ howtousegit.py
@@ -1,5 +1,7 @@
 print("To grab a clone, simply type git clone ADDRESS while in the appropriate directory")
 print("Next use git add FILE to add changes or files by name")
+print("call also use git add . or git add -A to add all things in your directory")
 print("Commit that cool stuff with git commit -m \"MESSAGE ABOUT WHAT WAS CHANGED\"")  # still only on our machine
 print("Finally, use git push to sync it up with the github website")
 print("And you're done")
+print("You can also use git pull to grab the latest from github")
